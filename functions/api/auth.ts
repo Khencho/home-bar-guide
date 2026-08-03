dcd3121bc0b920d6c816eed330c65c54f451e111
Khencho <khencho@users.noreply.github.com>
2026-08-03 13:23:11 +0300

    Remove Functions, use Decap OAuth proxy (static-only build)

--- functions/api/auth.ts
+++
@@ -1,9 +0,0 @@
-import { requestOAuthHandshake, handleAuthRedirect } from './oauth-helper';
-
-export const onRequest = async (context) => {
-  const url = new URL(context.request.url);
-  if (url.searchParams.get('code')) {
-    return handleAuthRedirect(context);
-  }
-  return requestOAuthHandshake(context);
-};
